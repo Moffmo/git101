e6dedb5408d32ea959facdbda305748e4ec5c1a1
matthew.batten@intergen.co.nz <matthew.batten@intergen.co.nz>
2017-04-03 12:59:55 +1200

    Solution file

--- GitDemoLibrary/GitDemoLibrary/Class1.cs
+++ GitDemoLibrary/GitDemoLibrary/Class1.cs
@@ -8,5 +8,12 @@ namespace GitDemoLibrary
 {
     public class Class1
     {
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+        public string Prop3 { get; set; }
+        public string Prop4 { get; set; }
+        public string Prop5 { get; set; }
+        public string Prop6 { get; set; }
+        public string Prop7 { get; set; }
     }
 }
